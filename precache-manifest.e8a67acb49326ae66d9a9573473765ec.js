0a970055c8e3632a2c5ebbc0cafb8d5a53c66e5b
Slavik Petryk <slavik.petruk@gmail.com>
2021-10-13 20:09:44 +0300

    Updates

---
+++ precache-manifest.e8a67acb49326ae66d9a9573473765ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c82b429ba5f0d183d400ba44b6d946",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78a70ec611e32f7c3dfb",
+    "url": "/static/css/main.500cff94.chunk.css"
+  },
+  {
+    "revision": "57962469a27c4d5e6a4c",
+    "url": "/static/js/2.618b884e.chunk.js"
+  },
+  {
+    "revision": "78a70ec611e32f7c3dfb",
+    "url": "/static/js/main.bf6a74b0.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/static/media/up.551b5c6b.svg"
+  }
+]);
