fd6451384c2de268d7a32868d8368fff317dca00
Slavik Petryk <slavik.petruk@gmail.com>
2021-10-13 20:14:06 +0300

    Updates

---
+++ precache-manifest.587fd25aeb3780269c564a7b7ea18550.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a915541e6c23c46bd0a813835aeb0d4",
+    "url": "/HamburgerStore/index.html"
+  },
+  {
+    "revision": "7b4ca7b08559cdd1e204",
+    "url": "/HamburgerStore/static/css/main.6089cdbd.chunk.css"
+  },
+  {
+    "revision": "387c57d1d711b829ead8",
+    "url": "/HamburgerStore/static/js/2.00c4c231.chunk.js"
+  },
+  {
+    "revision": "7b4ca7b08559cdd1e204",
+    "url": "/HamburgerStore/static/js/main.8fbcd02c.chunk.js"
+  },
+  {
+    "revision": "a5c0f887c9bd6cc86024",
+    "url": "/HamburgerStore/static/js/runtime-main.5bdacc43.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/HamburgerStore/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/HamburgerStore/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/HamburgerStore/static/media/up.551b5c6b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8a67acb49326ae66d9a9573473765ec.js"
+  "/HamburgerStore/precache-manifest.587fd25aeb3780269c564a7b7ea18550.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HamburgerStore/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
